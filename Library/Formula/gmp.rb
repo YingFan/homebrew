913bd351c561e72c3fa85706531da1a9133dd419
Ingvar Tunell <yingfan68@gmail.com>
2010-03-25 19:48:18 +0800

    Update version and added macports fix to inline problem

--- Library/Formula/gmp.rb
+++ Library/Formula/gmp.rb
@@ -1,14 +1,19 @@
 require 'formula'
 
 class Gmp <Formula
-  url 'ftp://ftp.gnu.org/gnu/gmp/gmp-4.3.1.tar.bz2'
+  url 'ftp://ftp.gnu.org/gnu/gmp/gmp-5.0.1.tar.bz2'
   homepage 'http://gmplib.org/'
-  sha1 'acbd1edc61230b1457e9742136994110e4f381b2'
+  md5 '6bac6df75c192a13419dfd71d19240a7'
 
   def options
     [
       ["--skip-check", "Do not run 'make check' to verify libraries. (Not recommended.)"]
     ]
+  end
+
+  def pathes
+    # http://thread.gmane.org/gmane.comp.lib.gmp.bugs/1979
+    { :p0 => DATA }
   end
 
   def install
@@ -25,7 +30,6 @@ class Gmp <Formula
     
     system "./configure", *args
     system "make"
-    ENV.j1 # Don't install in parallel
     system "make install"
     
     # Different compilers and options can cause tests to fail even
@@ -33,3 +37,25 @@ class Gmp <Formula
     system "make check" unless ARGV.include? "--skip-check"
   end
 end
+
+__END__
+--- gmp-h.in.orig       2010-01-07 13:09:02.000000000 -0700
++++ gmp-h.in    2010-01-27 19:35:31.000000000 -0700
+@@ -421,13 +421,14 @@
+     GCC 4.3 and above with -std=c99 or -std=gnu99 implements ISO C99
+     inline semantics, unless -fgnu89-inline is used.  */
+ #ifdef __GNUC__
+-#if (defined __GNUC_STDC_INLINE__) || (__GNUC__ == 4 && __GNUC_MINOR__ == 2)
++#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 2)
+ #define __GMP_EXTERN_INLINE extern __inline__ __attribute__ ((__gnu_inline__))
+-#else
++#define __GMP_INLINE_PROTOTYPES  1
++#elif !(defined __APPLE__ && defined __MACH__ && __APPLE_CC__ >= 5465 && defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L)
+ #define __GMP_EXTERN_INLINE      extern __inline__
+-#endif
+ #define __GMP_INLINE_PROTOTYPES  1
+ #endif
++#endif
+ 
+ /* DEC C (eg. version 5.9) supports "static __inline foo()", even in -std1
+    strict ANSI mode.  Inlining is done even when not optimizing (ie. -O0
