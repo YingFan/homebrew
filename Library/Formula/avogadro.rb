d331c761c0a31e35059f85a2f24b9019ba971926
Ingvar Tunell <yingfan68@gmail.com>
2010-03-31 00:11:59 +0800

    syntax

--- Library/Formula/avogadro.rb
+++ Library/Formula/avogadro.rb
@@ -4,23 +4,22 @@ class Avogadro <Formula
   url 'http://downloads.sourceforge.net/project/avogadro/avogadro/1.0.0/avogadro-1.0.0.tar.bz2'
   homepage 'http://avogadro.openmolecules.net'
   md5 '1146e8ca6cafa558a373d32f38680bcf'
-
+  
   depends_on 'cmake'
   depends_on 'qt'
   depends_on 'eigen'
   depends_on 'openbabel'
-
-  eigen = Formula.factory 'eigen'
-  openbabel = Formula.factory 'openbabel'
   
-
   def install
-   FileUtils.mkdir('build')
-   FileUtils.cd('build')
+    eigen = Formula.factory 'eigen'
+    openbabel = Formula.factory 'openbabel'
 
-   cmake_args = "-DOPENBABEL2_LIBRARIES=#{openbabel.lib} " + "-DOPENBABEL2_INCLUDE_DIR=#{openbabel.include} "
+    FileUtils.mkdir('build')
+    FileUtils.cd('build')
+    
+    cmake_args = "-DOPENBABEL2_LIBRARIES=#{openbabel.lib} " + "-DOPENBABEL2_INCLUDE_DIR=#{openbabel.include} "
               + "-DEIGEN2_INCLUDE_DIR=#{eigen.include} " + "-DDISABLE_UPDATE_CHECKER "
-
+    
     system "cmake .. #{std_cmake_parameters} " + cmake_args
     system "make install"
   end
